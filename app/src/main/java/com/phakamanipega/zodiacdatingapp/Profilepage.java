15cdea17d13a7a39d2f701d983da52f75df53d91
phakamanip <ppega@berklee.net>
2019-07-18 14:09:26 -0400

    updates most of the classes. need to add xml objects

--- app/src/main/java/com/phakamanipega/zodiacdatingapp/Profilepage.java
+++ app/src/main/java/com/phakamanipega/zodiacdatingapp/Profilepage.java
@@ -13,13 +13,10 @@ import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class Profilepage extends AppCompatActivity implements View.OnClickListener {
+public class Profilepage extends AppCompatActivity {
 
-    RadioGroup radiogroup;
-    RadioButton radioButton;
-    TextView textViewemail;
-    Button button;
-    public Button loggingout;
+    TextView SuntxtView, MoontxtView, RisingtxtView;
+    public Button button_Matches, button_Messages, button_Logout;
     private FirebaseAuth firebaseAuth;
 
 
@@ -28,60 +25,60 @@ public class Profilepage extends AppCompatActivity implements View.OnClickListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
 
+
+        SuntxtView = findViewById(R.id.SuntxtView);
+        MoontxtView = findViewById( R.id.MoontxtView );
+        RisingtxtView = findViewById( R.id.RisingtxtView );
+        button_Logout = findViewById( R.id.button_Logout );
+
         firebaseAuth = FirebaseAuth.getInstance();
+
+
         if(firebaseAuth.getCurrentUser() == null){
             finish();
             startActivity(new Intent(Profilepage.this, Loginpage.class));
         }
 
         FirebaseUser user = firebaseAuth.getCurrentUser();
-        textViewemail.setText("Welcome " + user.getEmail());
-
-        radiogroup = findViewById(R.id.Firesigns);
-        radiogroup = findViewById(R.id.Watersigns);
-        radiogroup = findViewById(R.id.Airsigns);
-        radiogroup = findViewById(R.id.Earthsigns);
-              textViewemail = findViewById(R.id.SuntxtView );
-       // button = findViewById(R.id.look);
-            //  loggingout = findViewById(R.id.logoutbtn);
+      //  textViewemail.setText("Welcome " + user.getEmail());
 
 
 
-
-        button.setOnClickListener(new View.OnClickListener() {
+        button_Matches.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int radioId = radiogroup.getCheckedRadioButtonId();
-                radioButton = findViewById(radioId);
-                 textViewemail.setText("Your choice: " + radioButton.getText());
+                Intent matchesActivity = new Intent( Profilepage.this, MatchesActivity.class );
+                startActivity( matchesActivity );
+            }
+        } );
+
+
+
+        button_Messages.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent chattingActivity = new Intent(Profilepage.this, ChattingActivity.class );
+                startActivity( chattingActivity );
 
             }
-        });
+        } );
 
 
 
 
+        button_Logout.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.signOut();
+                finish();
+                startActivity(new Intent(Profilepage.this, Loginpage.class));
 
-    }
-
-
-    public void checkbutton(View view){
-        int radioId = radiogroup.getCheckedRadioButtonId();
-
-        radioButton = findViewById(radioId);
-        Toast.makeText(this, "Selected Radio Button:" + radioButton.getText(), Toast.LENGTH_LONG).show();
-
-    }
-
-
-    @Override
-    public void onClick(View view) {
-
-        if(view == loggingout){
-            firebaseAuth.signOut();
-            finish();
-            startActivity(new Intent(Profilepage.this, Loginpage.class));
-        }
+            }
+        } );
 
     }
+
+
+
+
 }
--- app/src/main/java/com/phakamanipega/zodiacdatingapp/Signupage.java
+++ app/src/main/java/com/phakamanipega/zodiacdatingapp/Signupage.java
@@ -1,38 +1,26 @@
 package com.phakamanipega.zodiacdatingapp;
 
 
-import
-android.app.Activity;
-import android.app.ProgressDialog;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.text.TextUtils;
 import android.view.View;
-import android.widget.Adapter;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.Spinner;
 import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import org.w3c.dom.Text;
 
 
-public class Signupage extends Activity implements View.OnClickListener, AdapterView.OnItemSelectedListener{
-    ProgressBar progressBar;
-    public EditText password;
-    public EditText nickname;
-    public EditText dob;
-    public EditText email;
+public class Signupage extends Activity  {
+
+    public EditText email, nickname, dateofbirth, password, confirmpassword;
     public Button save;
     public Button back;
-    public ProgressDialog progressDialog;
     public FirebaseAuth firebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
 
@@ -55,8 +43,6 @@ public class Signupage extends Activity implements View.OnClickListener, Adapter
         };
 
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.months, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
 
 
@@ -69,34 +55,38 @@ public class Signupage extends Activity implements View.OnClickListener, Adapter
 
 
 
-        save.setOnClickListener(this);
-        back.setOnClickListener(this);
-
-        save.setOnClickListener(new View.OnClickListener() {
+        save.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 registerUser();
 
-//                Intent loggedin = new Intent(Signupage.this, Loginpage.class);
-//                startActivity(loggedin);
+                Intent gotoProfile = new Intent( Signupage.this, Profilepage.class );
+                startActivity( gotoProfile );
 
             }
-        });
+        } );
+
+        back.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotoMain = new Intent( Signupage.this, MainActivity.class );
+                startActivity( gotoMain );
+            }
+        } );
+
+
+
+
+
+
     }
 
 
 
-       public void Zsigns(){
 
 
-        String Aquarius = dob.getText().toString().trim();
 
-        if(dob.getText().toString()== "11 January, 30 December"){
-            Toast.makeText(this, "Welcome Aquarius", Toast.LENGTH_LONG).show();
-
-        }
-
-        }
 
 
         public void onStart(){
@@ -112,7 +102,8 @@ public class Signupage extends Activity implements View.OnClickListener, Adapter
             String NICKNAME = nickname.getText().toString().trim();
             String PASSWORD = password.getText().toString().trim();
             String EMAIL = email.getText().toString().trim();
-            String DOB = dob.getText().toString().trim();
+            String DOB = dateofbirth.getText().toString().trim();
+            String CNFPASSWORD = confirmpassword.getText().toString().trim();
 
             if (TextUtils.isEmpty(PASSWORD)) {//stops further action
                 Toast.makeText(this, "please enter PASSWORD", Toast.LENGTH_LONG).show();
@@ -130,17 +121,17 @@ public class Signupage extends Activity implements View.OnClickListener, Adapter
                 Toast.makeText(this, "please enter EMAIL", Toast.LENGTH_LONG).show();
                 return;
             }
+            if (TextUtils.isEmpty( CNFPASSWORD )){
+                Toast.makeText( this, "please confirm your password", Toast.LENGTH_LONG).show();
+            }
 
-
-                progressBar.setVisibility(View.VISIBLE);
-                progressDialog.show();
+;
 
                 firebaseAuth.createUserWithEmailAndPassword(EMAIL, PASSWORD)
                         .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {//user entered successfully
-                                    progressBar.setVisibility(View.GONE);
                                         Toast.makeText(Signupage.this, "Well done, ure registersted", Toast.LENGTH_LONG).show();
 
 
@@ -156,30 +147,12 @@ public class Signupage extends Activity implements View.OnClickListener, Adapter
             }
 
 
-    @Override
-    public void onClick(View v) {
-        if(v == save){
 
-            registerUser();
-        }
 
-        if(v == back) {
-
-            startActivity(new Intent(Signupage.this, MainActivity.class));
-            finish();
-
-        }
 
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String text = parent.getItemAtPosition(position).toString();
-        Toast.makeText(parent.getContext(), text,Toast.LENGTH_SHORT).show();
-    }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
 
-    }
-}
+
+
